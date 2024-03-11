87c0c4cdaf4bf829b634938998ad287093570546
Nikita Belov <nikitabelov.net@gmail.com>
2024-03-11 21:31:03 +0400

    Past some modules
    "Interact with data in Blazor web apps" module was finished

---
+++ Data/PizzaService.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazingPizza.Data
+{
+    public class PizzaService
+    {
+        public Task<Pizza[]> GetPizzasAsync()
+        {
+            // Call your data access technology here
+            var pl = new Pizza[] { new() { Name = "a", Description = "aaa", PizzaId = 1, Price = 1, Vegan = false, Vegetarian = false } };
+            return Task<Pizza[]>.FromResult(pl); ;
+        }
+    }
+}
